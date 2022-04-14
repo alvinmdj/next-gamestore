7498b2e6b60fc0f3bc073f9bc1904e37ae95fa4c
Alvin <alvinmartindwrk@gmail.com>
2022-04-14 22:52:36 +0700

    refactor login navbar component

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -1,30 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import Auth from './Auth';
 import Menu from './Menu';
+import ToggleMenu from './ToggleMenu';
 
 const Navbar: React.FC = () => {
   return (
     <section>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <Image src='/icon/logo.svg' alt='logo' width={60} height={60} />
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
+      <nav className='navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50'>
+        <div className='container-fluid'>
+          <Link href='/'>
+            <a className='navbar-brand'>
+              <Image src='/icon/logo.svg' alt='logo' width={60} height={60} />
+            </a>
+          </Link>
+          <ToggleMenu />
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ms-auto text-lg gap-lg-0 gap-2'>
               <Menu title='Home' active />
               <Menu title='Games' href='/games' />
               <Menu title='Rewards' href='/rewards' />
               <Menu title='Discover' href='/discover' />
               <Menu title='Global Rank' href='/global-rank' />
-              <li className="nav-item my-auto">
-                <a className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-                  href="./src/sign-in.html" role="button">Sign In</a>
-              </li>
+              <Auth isLoggedIn />
             </ul>
           </div>
         </div>
