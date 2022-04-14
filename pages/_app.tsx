16cf1f39533bb884058f3aef7694712b09dafa1b
Alvin <alvinmartindwrk@gmail.com>
2022-04-14 21:37:22 +0700

    convert member transactions detail page from html to nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import '../styles/not-found.css';
 import '../styles/sidebar.css';
 import '../styles/overview.css';
 import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';;
 
