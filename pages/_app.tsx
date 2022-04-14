8561f5f8a57da784c8f6a6e229ea8bb50226c3e9
Alvin <alvinmartindwrk@gmail.com>
2022-04-14 21:29:32 +0700

    convert member overview page from html to nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,8 @@ import '../styles/sign-up.css';
 import '../styles/sign-up-photo.css';
 import '../styles/sign-up-success.css';
 import '../styles/not-found.css';
+import '../styles/sidebar.css';
+import '../styles/overview.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';;
 
