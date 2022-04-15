aca9ecd9e127e570e437259f85c5aee5d1533e58
Alvin <alvinmartindwrk@gmail.com>
2022-04-15 16:23:42 +0700

    refactor authentication pages

---
+++ components/organisms/SignInIllustration/index.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+
+const SignInIllustration = () => {
+  return (
+    <div className='col-xxl-7 col-lg-6 bg-blue text-center pt-lg-145 pb-lg-145 d-lg-block d-none'>
+      <img src='/img/Header-9.png' width='502' height='391.21' className='img-fluid pb-50' alt='' />
+      <h2 className='text-4xl fw-bold text-white mb-30'>
+        Win the battle.
+        <br />
+        Be the Champion.
+      </h2>
+      <p className='text-white m-0'>
+        We provide many ways
+        <br />
+        {' '}
+        to assist gamers become the best
+      </p>
+    </div>
+  );
+};
+
+export default SignInIllustration;
