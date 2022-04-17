9c9a5be19ded041b1bdd613026da88e9ea435290
Alvin <alvinmartindwrk@gmail.com>
2022-04-17 12:29:35 +0700

    add global styles in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import '../styles/utilities.css';
+import '../styles/homepage.css';
+import '../styles/detail.css';
+import '../styles/checkout.css';
+import '../styles/complete-checkout.css';
+import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-success.css';
+import '../styles/not-found.css';
+import '../styles/sidebar.css';
+import '../styles/overview.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
+import '../styles/navbar-log-in.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
