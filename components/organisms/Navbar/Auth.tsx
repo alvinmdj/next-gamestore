6da64af994bccd3cfc09e52d42c7fdcfa2c30ff9
Alvin <alvinmartindwrk@gmail.com>
2022-04-30 15:26:34 +0700

    navbar login condition & adjust default avatar

--- components/organisms/Navbar/Auth.tsx
+++ components/organisms/Navbar/Auth.tsx
@@ -1,12 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
+import Cookies from 'js-cookie';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 
-interface AuthProps {
-  isLoggedIn?: boolean;
+interface JwtPayloadTypes {
+  player: {
+    avatar: string;
+  }
 }
 
-const Auth = (props: Partial<AuthProps>) => {
-  const { isLoggedIn } = props;
+const Auth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({ avatar: '' });
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = Buffer.from(token, 'base64').toString('utf-8'); // atob is deprecated
+      const payload: JwtPayloadTypes = jwt_decode(jwtToken);
+      const user = payload.player;
+      if (user.avatar) {
+        const IMG_ROOT = process.env.NEXT_PUBLIC_IMG;
+        user.avatar = `${IMG_ROOT}/${user.avatar}`;
+      }
+      setIsLoggedIn(true);
+      setUser(user);
+    }
+  }, []);
 
   if (isLoggedIn) {
     return (
@@ -16,7 +37,7 @@ const Auth = (props: Partial<AuthProps>) => {
           <a className='dropdown-toggle ms-lg-40' href='#' role='button' id='dropdownMenuLink'
             data-bs-toggle='dropdown' aria-expanded='false'>
             <Image
-              src='/img/avatar-1.png'
+              src={user.avatar ? user.avatar : '/img/avatar-1.png'}
               className='rounded-circle'
               width='40'
               height='40'
@@ -25,7 +46,6 @@ const Auth = (props: Partial<AuthProps>) => {
           </a>
           <ul className='dropdown-menu border-0' aria-labelledby='dropdownMenuLink'>
             <li><Link href='/member'><a className='dropdown-item text-lg color-palette-2'>My Profile</a></Link></li>
-            {/* <li><Link href='/'><a className='dropdown-item text-lg color-palette-2'>Wallet</a></Link></li> */}
             <li><Link href='/member/edit-profile'><a className='dropdown-item text-lg color-palette-2'>Account Settings</a></Link></li>
             <li><Link href='/sign-in'><a className='dropdown-item text-lg color-palette-2'>Log Out</a></Link></li>
           </ul>
@@ -36,8 +56,9 @@ const Auth = (props: Partial<AuthProps>) => {
   return (
     <li className='nav-item my-auto'>
       <Link href='/sign-in'>
-        <a className='btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill'
-          role='button'>Sign In</a>
+        <a className='btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill'>
+          Sign In
+        </a>
       </Link>
     </li>
   );
