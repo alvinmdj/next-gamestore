9e58f6b029ee1b8ab2987fbf8cde53f11e12278b
Alvin <alvinmartindwrk@gmail.com>
2022-04-30 16:06:21 +0700

    handling private route (checkout page) with getServerSideProps

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,11 +1,44 @@
+import jwtDecode from 'jwt-decode'
+import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import CheckoutConfirmation from '../components/organisms/CheckoutConfirmation'
 import CheckoutDetail from '../components/organisms/CheckoutDetail'
 import CheckoutItem from '../components/organisms/CheckoutItem'
+import { JWTPayloadTypes, UserTypes } from '../services/data-types'
 
-const Checkout: React.FC = () => {
+interface CheckoutProps {
+  user: UserTypes;
+};
+
+// * note: console.log in getServerSideProps will appear in terminal
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { token } = req.cookies; // get token from cookie (server side)
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+  const jwtToken = Buffer.from(token, 'base64').toString('utf-8');
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+  const userFromPayload: UserTypes = payload.player;
+  if (userFromPayload.avatar) {
+    const IMG_ROOT = process.env.NEXT_PUBLIC_IMG;
+    userFromPayload.avatar = `${IMG_ROOT}/${userFromPayload.avatar}`;
+  }
+  return {
+    props: {
+      user: userFromPayload,
+    },
+  };
+};
+
+const Checkout = (props: CheckoutProps) => {
+  const { user } = props;
+
   return (
     <section className='checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30'>
       <div className='container-fluid'>
@@ -27,4 +60,4 @@ const Checkout: React.FC = () => {
   )
 }
 
-export default Checkout
+export default Checkout;
