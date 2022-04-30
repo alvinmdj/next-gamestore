d8093bf073c84d66104202773e0b9bf49125bcfa
Alvin <alvinmartindwrk@gmail.com>
2022-04-30 21:07:44 +0700

    integrate checkout item & checkout detail data

--- components/organisms/CheckoutDetail/index.tsx
+++ components/organisms/CheckoutDetail/index.tsx
@@ -1,29 +1,134 @@
+import { useEffect, useState } from "react";
+import NumberFormat from "react-number-format";
+
 const CheckoutDetail = () => {
+  const [topUpData, setTopUpData] = useState({
+    verifyID: '',
+    nominalItem: {
+      _id: '',
+      coinName: '',
+      coinQuantity: 0,
+      price: 0,
+    },
+    paymentItem: {
+      payment: {
+        _id: '',
+        type: '',
+      },
+      bank: {
+        _id: '',
+        bankName: '',
+        accountNumber: '',
+        name: '',
+      },
+    },
+    bankAccountName: '',
+  });
+
+  useEffect(() => {
+    const dataFromLocal = localStorage.getItem('top-up-data');
+    const localTopUpData = JSON.parse(dataFromLocal!);
+    setTopUpData(localTopUpData);
+  }, []);
+
+  const itemPrice = topUpData.nominalItem.price;
+  const tax = topUpData.nominalItem.price * (10 / 100);
+  const totalPrice = itemPrice + tax;
+
   return (
     <>
       <div className='purchase pt-md-50 pt-30'>
-        <h2 className='fw-bold text-xl color-palette-1 mb-20'>Purchase Details</h2>
-        <p className='text-lg color-palette-1 mb-20'>Your Game ID <span
-          className='purchase-details'>masayoshizero</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Order ID <span className='purchase-details'>#GG001</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Item <span className='purchase-details'>250 Diamonds</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Price <span className='purchase-details'>Rp 42.280.500</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Tax (10%) <span className='purchase-details'>Rp 4.228.000</span>
+        <h2 className='fw-bold text-xl color-palette-1 mb-20'>
+          Purchase Details
+        </h2>
+        <p className='text-lg color-palette-1 mb-20'>
+          Your Game ID
+          <span className='purchase-details'>
+            {topUpData.verifyID}
+          </span>
         </p>
-        <p className='text-lg color-palette-1 mb-20'>Total <span className='purchase-details color-palette-4'>Rp
-          55.000.600</span></p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Order ID
+          <span className='purchase-details'>#GG001</span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Item
+          <span className='purchase-details'>
+            {topUpData.nominalItem.coinQuantity}
+            {' '}
+            {topUpData.nominalItem.coinName}
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Price
+          <span className='purchase-details'>
+            <NumberFormat
+              value={itemPrice}
+              displayType='text'
+              thousandSeparator={'.'}
+              decimalSeparator={','}
+              prefix='Rp '
+            />
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Tax (10%)
+          <span className='purchase-details'>
+            <NumberFormat
+              value={tax}
+              displayType='text'
+              thousandSeparator={'.'}
+              decimalSeparator={','}
+              prefix='Rp '
+            />
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Total
+          <span className='purchase-details color-palette-4'>
+            <NumberFormat
+              value={totalPrice}
+              displayType='text'
+              thousandSeparator={'.'}
+              decimalSeparator={','}
+              prefix='Rp '
+            />
+          </span>
+        </p>
       </div>
       <div className='payment pt-md-50 pb-md-50 pt-10 pb-10'>
-        <h2 className='fw-bold text-xl color-palette-1 mb-20'>Payment Informations</h2>
-        <p className='text-lg color-palette-1 mb-20'>Your Account Name <span className='purchase-details'>Masayoshi
-          Angga Zero</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Type <span className='payment-details'>Worldwide Transfer</span>
+        <h2 className='fw-bold text-xl color-palette-1 mb-20'>
+          Payment Informations
+        </h2>
+        <p className='text-lg color-palette-1 mb-20'>
+          Your Account Name
+          <span className='purchase-details'>
+            {topUpData.bankAccountName}
+          </span>
         </p>
-        <p className='text-lg color-palette-1 mb-20'>Bank Name <span className='payment-details'>Mandiri</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Bank Account Name <span className='payment-details'>PT Store GG
-          Indonesia</span></p>
-        <p className='text-lg color-palette-1 mb-20'>Bank Number <span className='payment-details'>1800 - 9090 -
-          2021</span>
+        <p className='text-lg color-palette-1 mb-20'>
+          Type
+          <span className='payment-details'>
+            {topUpData.paymentItem.payment.type}
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Bank Name
+          <span className='payment-details'>
+            {topUpData.paymentItem.bank.bankName}
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Bank Account Name
+          <span className='payment-details'>
+            {topUpData.paymentItem.bank.name}
+          </span>
+        </p>
+        <p className='text-lg color-palette-1 mb-20'>
+          Bank Number
+          <span className='payment-details'>
+            {topUpData.paymentItem.bank.accountNumber}
+          </span>
         </p>
       </div>
     </>
--- components/organisms/CheckoutItem/index.tsx
+++ components/organisms/CheckoutItem/index.tsx
@@ -1,15 +1,39 @@
+import { useEffect, useState } from "react";
+
 /* eslint-disable @next/next/no-img-element */
 const CheckoutItem = () => {
+  const [voucherData, setVoucherData] = useState({
+    thumbnail: '',
+    name: '',
+    category: {
+      name: '',
+    },
+  });
+
+  const IMG_ROOT = process.env.NEXT_PUBLIC_IMG;
+
+  useEffect(() => {
+    const dataFromLocal = localStorage.getItem('voucher-data');
+    const localVoucherData = JSON.parse(dataFromLocal!);
+    setVoucherData(localVoucherData);
+  }, []);
+
   return (
     <div className='game-checkout d-flex flex-row align-items-center pt-md-50 pb-md-50 pt-30 pb-30'>
       <div className='pe-4'>
         <div className='cropped'>
-          <img src='/img/Thumbnail-3.png' className='img-fluid' alt='' />
+          <img src={`${IMG_ROOT}/${voucherData.thumbnail}`} className='img-fluid' alt={voucherData.name} />
         </div>
       </div>
       <div>
-        <p className='fw-bold text-xl color-palette-1 mb-10'>Mobile Legends:<br /> The New Battle 2021</p>
-        <p className='color-palette-2 m-0'>Category: Mobile</p>
+        <p className='fw-bold text-xl color-palette-1 mb-10'>
+          {voucherData.name}
+        </p>
+        <p className='color-palette-2 m-0'>
+          Category:
+          {' '}
+          {voucherData.category.name}
+        </p>
       </div>
     </div>
   );
