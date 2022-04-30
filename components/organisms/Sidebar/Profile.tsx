4246ca978dafca7e6b23f1a34d8e3f0fc422ad21
Alvin <alvinmartindwrk@gmail.com>
2022-04-30 22:11:59 +0700

    integrate sidebar profile user data

--- components/organisms/Sidebar/Profile.tsx
+++ components/organisms/Sidebar/Profile.tsx
@@ -1,10 +1,41 @@
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
+import { JWTPayloadTypes, UserTypes } from "../../../services/data-types";
+
 /* eslint-disable @next/next/no-img-element */
 const Profile = () => {
+  const [user, setUser] = useState({
+    avatar: '',
+    name: '',
+    email: '',
+  });
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = Buffer.from(token, 'base64').toString('utf-8');
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userFromPayload: UserTypes = payload.player;
+      if (userFromPayload.avatar) {
+        const IMG_ROOT = process.env.NEXT_PUBLIC_IMG;
+        userFromPayload.avatar = `${IMG_ROOT}/${userFromPayload.avatar}`;
+      }
+      setUser(userFromPayload);
+    }
+  }, []);
+
   return (
     <div className='user text-center pb-50 pe-30'>
-      <img src='/img/avatar-1.png' width='90' height='90' className='img-fluid mb-20' alt='' />
-      <h2 className='fw-bold text-xl color-palette-1 m-0'>Shayna Anne</h2>
-      <p className='color-palette-2 m-0'>shayna@anne.com</p>
+      <img
+        src={user.avatar ? user.avatar : '/img/avatar-1.png'}
+        width='90'
+        height='90'
+        className='img-fluid mb-20'
+        style={{ borderRadius: '50%' }}
+        alt='avatar'
+      />
+      <h2 className='fw-bold text-xl color-palette-1 m-0'>{user.name}</h2>
+      <p className='color-palette-2 m-0'>{user.email}</p>
     </div>
   );
 };
