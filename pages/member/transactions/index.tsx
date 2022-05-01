4e74a38fbda5a0993c5ee1c22e34860b836b7c77
Alvin <alvinmartindwrk@gmail.com>
2022-05-01 22:27:15 +0700

    remove user data props from getServerSideProps return value (not needed)

--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -1,8 +1,6 @@
-import jwtDecode from 'jwt-decode';
 import { GetServerSideProps } from 'next';
 import Sidebar from '../../../components/organisms/Sidebar';
 import TransactionContent from '../../../components/organisms/TransactionContent';
-import { JWTPayloadTypes, UserTypes } from '../../../services/data-types';
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const { token } = req.cookies; // get token from cookie (server side)
@@ -14,16 +12,10 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
       },
     };
   }
-  const jwtToken = Buffer.from(token, 'base64').toString('utf-8');
-  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
-  const userFromPayload: UserTypes = payload.player;
-  if (userFromPayload.avatar) {
-    const IMG_ROOT = process.env.NEXT_PUBLIC_IMG;
-    userFromPayload.avatar = `${IMG_ROOT}/${userFromPayload.avatar}`;
-  }
+
   return {
     props: {
-      user: userFromPayload,
+      user: {},
     },
   };
 };
