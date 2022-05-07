dddbd62f29e58287a5bac017b253d0d872b3753f
Alvin <alvinmartindwrk@gmail.com>
2022-05-07 21:22:08 +0700

    add page load progress bar with nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,16 +20,18 @@ import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Script from 'next/script';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Call Bootstrap JS */}
       <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
-        crossOrigin="anonymous"
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
+        integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
+        crossOrigin='anonymous'
       />
+      <NextNProgress color='#263fcc' />
       <Component {...pageProps} />
       <ToastContainer />
     </>
